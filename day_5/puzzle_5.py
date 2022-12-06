1c54d2cef717f091eb6f8267fc47c5f733090571
Gerardo Ayes <gerardoayes92@gmail.com>
2022-12-05 20:27:24 -0500

    going to create data in a different way.

--- day_5/puzzle_5.py
+++ day_5/puzzle_5.py
@@ -8,19 +8,18 @@ class Solution:
 
         with open(self.file) as file:
             for line in file:
-                my_str = line.translate(
-                    str.maketrans("", "", "abcdefghijklmnopqrstuvwxyz\n[]")
+                self.data.append(
+                    line.translate(
+                        str.maketrans("", "", "abcdefghijklmnopqrstuvwxyz\n")
+                    )
                 )
-                self.data.append(re.sub(" +", " ", my_str))
 
         p_i = 0
         while self.data[p_i] != "":
             p_i += 1
 
         self.instructions = self.data[p_i + 1 :]
-        self.data = self.data[:p_i]
-        self.data[-1] = self.data[-1].strip()
-        self.n = int(self.data[-1][-1])
+        self.data = self.data[: p_i - 1]
 
         for i in range(len(self.instructions)):
             self.instructions[i] = self.instructions[i].strip()
@@ -28,12 +27,17 @@ class Solution:
         print(self.data)
         print(self.instructions)
 
+        for i in range(len(self.data), -1, -1):
+            
+
     def solve_part_1(self) -> int:
         pass
 
     def solve_part_2(self) -> int:
         pass
 
+
+# ["ZN", "MCD", "P"]
 
 # answer = Solution("day_5/puzzle_5_data.txt")
 # print("Solution to Puzzle 5 Part 1: ", answer.solve_part_1())
