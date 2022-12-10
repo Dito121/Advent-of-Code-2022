70b55746991c535c7b37c8d9c764a4b64f956079
Gerardo Ayes <gerardoayes92@gmail.com>
2022-12-09 23:49:41 -0500

    Solved Puzzle 9 Part 1. Working on Puzzle 9 Part 2.

--- day_9/puzzle_9.py
+++ day_9/puzzle_9.py
@@ -11,8 +11,14 @@ class Solution:
                 line = line.strip().split()
                 self.data.append([line[0], int(line[1])])
 
-    def are_touching(self, p1: list[int], p2: list[int]) -> bool:
-        return int(np.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)) < 2
+    def are_touching(self) -> bool:
+        return (
+            np.sqrt(
+                (self.p_head[0] - self.p_tail[0]) ** 2
+                + (self.p_head[1] - self.p_tail[1]) ** 2
+            )
+            < 2
+        )
 
     def track_tail(self):
         if tuple(self.p_tail) not in self.path:
@@ -22,39 +28,46 @@ class Solution:
 
     def shift_tail(self, i):
         if self.data[i][0] == "R":
-            for _ in range(self.data[i][1]):
-                self.p_head[1] += 1
-                if not self.are_touching(self.p_head, self.p_tail):
-                    self.p_tail[1] += 1
-                    self.track_tail()
+            self.p_head[1] += 1
+
+            if not self.are_touching():
+                if self.p_tail[0] != self.p_head[0]:
+                    self.p_tail[0] = self.p_head[0]
+                self.p_tail[1] += 1
 
         elif self.data[i][0] == "L":
-            for _ in range(self.data[i][1]):
-                self.p_head[1] -= 1
-                if not self.are_touching(self.p_head, self.p_tail):
-                    self.p_tail[1] -= 1
-                    self.track_tail()
+            self.p_head[1] -= 1
+
+            if not self.are_touching():
+                if self.p_tail[0] != self.p_head[0]:
+                    self.p_tail[0] = self.p_head[0]
+                self.p_tail[1] -= 1
 
         elif self.data[i][0] == "U":
-            for _ in range(self.data[i][1]):
-                self.p_head[0] += 1
-                if not self.are_touching(self.p_head, self.p_tail):
-                    self.p_tail[0] += 1
-                    self.track_tail()
+            self.p_head[0] -= 1
+
+            if not self.are_touching():
+                if self.p_tail[1] != self.p_head[1]:
+                    self.p_tail[1] = self.p_head[1]
+                self.p_tail[0] -= 1
 
         elif self.data[i][0] == "D":
-            for _ in range(self.data[i][1]):
-                self.p_head[0] -= 1
-                if not self.are_touching(self.p_head, self.p_tail):
-                    self.p_tail[0] -= 1
-                    self.track_tail()
+            self.p_head[0] += 1
+
+            if not self.are_touching():
+                if self.p_tail[1] != self.p_head[1]:
+                    self.p_tail[1] = self.p_head[1]
+                self.p_tail[0] += 1
 
     def solve_part_1(self):
-        self.p_head = self.p_tail = [len(self.data) - 1, 0]
+        self.p_head = [len(self.data) - 1, 0]
+        self.p_tail = [len(self.data) - 1, 0]
         self.path = {tuple(self.p_tail): 1}
 
         for i in range(len(self.data)):
-            self.shift_tail(i)
+            for _ in range(self.data[i][1]):
+                self.shift_tail(i)
+                self.track_tail()
 
         return len(self.path)
 
@@ -62,6 +75,6 @@ class Solution:
         pass
 
 
-# answer = Solution("day_9/puzzle_9_data.txt")
-# print("Solution to Puzzle 9 Part 1: ", answer.solve_part_1())
+answer = Solution("day_9/puzzle_9_data.txt")
+print("Solution to Puzzle 9 Part 1: ", answer.solve_part_1())
 # print("Solution to Puzzle 9 Part 2: ", answer.solve_part_2())
