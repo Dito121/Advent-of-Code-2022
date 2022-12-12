d05935230a4c07385ba98101718b8022007332a1
Gerardo Ayes <gerardoayes92@gmail.com>
2022-12-12 13:24:45 -0500

    generalized part 1, now going to try to test part 2

--- day_9/puzzle_9.py
+++ day_9/puzzle_9.py
@@ -2,67 +2,67 @@ import numpy as np
 
 
 class Solution:
-    def __init__(self, file: str):
+    def __init__(self, file: str, n: int = 2):
         self.file = file
         self.data = []
+        self.n = n
 
         with open(self.file) as file:
             for line in file:
                 line = line.strip().split()
                 self.data.append([line[0], int(line[1])])
 
-    def are_touching(self) -> bool:
-        return (
-            np.sqrt(
-                (self.p_head[0] - self.p_tail[0]) ** 2
-                + (self.p_head[1] - self.p_tail[1]) ** 2
-            )
-            < 2
-        )
+    def are_touching(self, p1: list[int, int], p2: list[int, int]) -> bool:
+        return np.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2) < 1.9
 
     def track_tail(self):
-        if tuple(self.p_tail) not in self.path:
-            self.path[tuple(self.p_tail)] = 1
+        if tuple(self.points[-1]) not in self.path:
+            self.path[tuple(self.points[-1])] = 1
         else:
-            self.path[tuple(self.p_tail)] += 1
+            self.path[tuple(self.points[-1])] += 1
 
     def shift_tail(self, i):
         if self.data[i][0] == "R":
-            self.p_head[1] += 1
+            self.points[0][1] += 1
 
-            if not self.are_touching():
-                if self.p_tail[0] != self.p_head[0]:
-                    self.p_tail[0] = self.p_head[0]
-                self.p_tail[1] += 1
+            for j in range(1, len(self.points)):
+                if not self.are_touching(self.points[j], self.points[j - 1]):
+                    if self.points[j][0] != self.points[j - 1][0]:
+                        self.points[j][0] = self.points[j - 1][0]
+                    self.points[j][1] += 1
 
         elif self.data[i][0] == "L":
-            self.p_head[1] -= 1
+            self.points[0][1] -= 1
 
-            if not self.are_touching():
-                if self.p_tail[0] != self.p_head[0]:
-                    self.p_tail[0] = self.p_head[0]
-                self.p_tail[1] -= 1
+            for j in range(1, len(self.points)):
+                if not self.are_touching(self.points[j], self.points[j - 1]):
+                    if self.points[j][0] != self.points[j - 1][0]:
+                        self.points[j][0] = self.points[j - 1][0]
+                    self.points[j][1] -= 1
 
         elif self.data[i][0] == "U":
-            self.p_head[0] -= 1
+            self.points[0][0] -= 1
 
-            if not self.are_touching():
-                if self.p_tail[1] != self.p_head[1]:
-                    self.p_tail[1] = self.p_head[1]
-                self.p_tail[0] -= 1
+            for j in range(1, len(self.points)):
+                if not self.are_touching(self.points[j], self.points[j - 1]):
+                    if self.points[j][1] != self.points[j - 1][1]:
+                        self.points[j][1] = self.points[j - 1][1]
+                    self.points[j][0] -= 1
 
         elif self.data[i][0] == "D":
-            self.p_head[0] += 1
+            self.points[0][0] += 1
 
-            if not self.are_touching():
-                if self.p_tail[1] != self.p_head[1]:
-                    self.p_tail[1] = self.p_head[1]
-                self.p_tail[0] += 1
+            for j in range(1, len(self.points)):
+                if not self.are_touching(self.points[j], self.points[j - 1]):
+                    if self.points[j][1] != self.points[j - 1][1]:
+                        self.points[j][1] = self.points[j - 1][1]
+                    self.points[j][0] += 1
 
     def solve_part_1(self):
-        self.p_head = [len(self.data) - 1, 0]
-        self.p_tail = [len(self.data) - 1, 0]
-        self.path = {tuple(self.p_tail): 1}
+        self.points = []
+        for i in range(self.n):
+            self.points.append([len(self.data) - 1, 0])
+        self.path = {tuple(self.points[-1]): 1}
 
         for i in range(len(self.data)):
             for _ in range(self.data[i][1]):
@@ -75,6 +75,6 @@ class Solution:
         pass
 
 
-answer = Solution("day_9/puzzle_9_data.txt")
-print("Solution to Puzzle 9 Part 1: ", answer.solve_part_1())
+# answer = Solution("day_9/puzzle_9_data.txt")
+# print("Solution to Puzzle 9 Part 1: ", answer.solve_part_1())
 # print("Solution to Puzzle 9 Part 2: ", answer.solve_part_2())
--- day_9/tests_puzzle_9.py
+++ day_9/tests_puzzle_9.py
@@ -4,7 +4,7 @@ from puzzle_9 import Solution
 
 class test_problems(unittest.TestCase):
     def test_puzzle_9(self):
-        answer = Solution("day_9/puzzle_9_sample_data.txt")
+        answer = Solution("day_9/puzzle_9_sample_data_part_1.txt")
 
         self.assertEqual(answer.solve_part_1(), 13)
         # self.assertEqual(answer.solve_part_2(), )
