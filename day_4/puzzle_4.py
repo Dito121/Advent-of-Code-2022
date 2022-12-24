b160a15391c22378c2323b67360e10c59b1692a0
Gerardo Ayes <gerardoayes92@gmail.com>
2022-12-24 10:20:40 -0500

    added comments to puzzle 4

--- day_4/puzzle_4.py
+++ day_4/puzzle_4.py
@@ -1,19 +1,29 @@
+import numpy as np
+
+
 class Solution:
     def __init__(self, file: str):
         self.file = file
         self.data = []
         self.answer = 0
+        self.read_file()
 
+    def read_file(self):
         with open(self.file, "r") as file:
             for line in file:
                 data = line.strip().split(",")
                 self.data.extend(
                     list(map(int, data[i].split("-"))) for i in range(len(data))
                 )
+        self.data = np.array(self.data)
 
     def solve_part_1(self) -> int:
         self.answer = 0
-
+        """
+        compare ith element of self.data with i+1 element. conditionals separate
+        logic for checking whether intersection applies. (in this case one needs
+        to be fully contained within the other)
+        """
         for i in range(0, len(self.data), 2):
             if (
                 self.data[i][0] <= self.data[i + 1][0]
@@ -30,7 +40,11 @@ class Solution:
 
     def solve_part_2(self) -> int:
         self.answer = 0
-
+        """
+        compare ith element of self.data with i+1 element. conditionals separate
+        logic for checking whether intersection applies. (in this case one needs
+        to be partially contained within the other)
+        """
         for i in range(0, len(self.data), 2):
             if (
                 (self.data[i][0] <= self.data[i + 1][0] <= self.data[i][1])
