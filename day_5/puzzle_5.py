272dde5cb8cd31d92794d4a233b20290c89b114d
Gerardo Ayes <gerardoayes92@gmail.com>
2022-12-29 15:54:30 -0500

    finished refactoring puzzle 5

--- day_5/puzzle_5.py
+++ day_5/puzzle_5.py
@@ -34,29 +34,37 @@ class Solution:
 
     def clean_instructions(self):
         for i in range(len(self.instructions)):
-            self.instructions[i] = (
-                self.instructions[i]
-                .translate(str.maketrans("", "", "abcdefghijklmnopqrstuvwxyz\n"))
-                .split()
+            self.instructions[i] = list(
+                map(
+                    int,
+                    self.instructions[i]
+                    .translate(str.maketrans("", "", "abcdefghijklmnopqrstuvwxyz\n"))
+                    .split(),
+                )
             )
 
     def get_stacks(self):
         return self.stacks.copy()
 
-    def move_item(self, from_stack, to_stack):
-        return
+    def move_item_n_times(self, stacks, n, from_stack, to_stack):
+        for _ in range(n):
+            stacks[to_stack] += stacks[from_stack][-1]
+            stacks[from_stack] = stacks[from_stack][:-1]
+
+    def move_n_items(self, stacks, n, from_stack, to_stack):
+        stacks[to_stack] += stacks[from_stack][-n:]
+        stacks[from_stack] = stacks[from_stack][:-n]
 
     def solve_part_1(self) -> int:
         stacks = self.get_stacks()
 
         for i in range(len(self.instructions)):
-            for _ in range(int(self.instructions[i][0])):
-                stacks[int(self.instructions[i][-1]) - 1] += stacks[
-                    int(self.instructions[i][1]) - 1
-                ][-1]
-                stacks[int(self.instructions[i][1]) - 1] = stacks[
-                    int(self.instructions[i][1]) - 1
-                ][:-1]
+            self.move_item_n_times(
+                stacks,
+                self.instructions[i][0],
+                self.instructions[i][1] - 1,
+                self.instructions[i][2] - 1,
+            )
 
         self.part1 = ""
         for i in range(len(stacks)):
@@ -68,12 +76,12 @@ class Solution:
         stacks = self.get_stacks()
 
         for i in range(len(self.instructions)):
-            stacks[int(self.instructions[i][2]) - 1] += stacks[
-                int(self.instructions[i][1]) - 1
-            ][-int(self.instructions[i][0]) :]
-            stacks[int(self.instructions[i][1]) - 1] = stacks[
-                int(self.instructions[i][1]) - 1
-            ][: -int(self.instructions[i][0])]
+            self.move_n_items(
+                stacks,
+                self.instructions[i][0],
+                self.instructions[i][1] - 1,
+                self.instructions[i][2] - 1,
+            )
 
         self.part2 = ""
         for i in range(len(stacks)):
@@ -82,6 +90,6 @@ class Solution:
         return self.part2
 
 
-# answer = Solution("day_5/puzzle_5_data.txt")
-# print("Solution to Puzzle 5 Part 1: ", answer.solve_part_1())
-# print("Solution to Puzzle 5 Part 2: ", answer.solve_part_2())
+answer = Solution("day_5/puzzle_5_data.txt")
+print("Solution to Puzzle 5 Part 1: ", answer.solve_part_1())
+print("Solution to Puzzle 5 Part 2: ", answer.solve_part_2())
