e0c6098ce38f8012d69aebf10a27100f5d6204a0
Gerardo Ayes <gerardoayes92@gmail.com>
2022-12-29 22:50:57 -0500

    partially refactored and added comments to puzzle 7

--- day_7/puzzle_7.py
+++ day_7/puzzle_7.py
@@ -6,51 +6,80 @@ class Solution:
         self.file = file
         self.data = []
         self.part1 = 0
+        self.max_space = 70_000_000
         self.space_needed = 30_000_000
+        self.read_file()
+        self.create_tree()
 
+    def read_file(self):
         with open(self.file) as file:
             self.data.extend(line.strip().split() for line in file)
-        self.root = Node("root", type="dir")
-        p_current = self.root
 
+    def create_children(self, p_right: int, p_current: Node):
+        """
+        Create a window from p_left to p_right that contains all files and
+        directories in p_current.
+        """
+        p_right += 1
+        p_left = p_right
+        while self.data[p_right][0] != "$" and p_right < len(self.data) - 1:
+            p_right += 1
+        """
+        Create nodes for each listed file and directory, set them as
+        children of p_current.
+        """
+        for data in self.data[p_left:p_right]:
+            if data[0] == "dir":
+                Node(
+                    data[1],
+                    parent=p_current,
+                    type="dir",
+                    size=0,
+                )
+            else:
+                node = Node(
+                    data[1],
+                    parent=p_current,
+                    type="file",
+                    size=int(data[0]),
+                )
+        return p_right  # return p_right or else won't register changes.
+
+    def cd_to_child(self, p_right: int, p_current: Node):
+        """
+        Create list of names of all child nodes and find index within
+        p_current.children so that we can cd into the correct child node.
+        """
+        children = [
+            child.name[: len(self.data[p_right][2])] for child in p_current.children
+        ]
+        index = children.index(self.data[p_right][2])
+        p_current = p_current.children[index]
+        return p_current  # return p_current or else won't register changes.
+
+    def create_tree(self):
+        """
+        Create a tree using anytree library and populate it when listing files
+        and directories within the current directory pointer (p_current).
+        Create a window using a left pointer (p_left) and right pointer (p_right).
+        """
+        self.root = Node("root", type="dir", size=0)
+        p_current = self.root
         p_right = 1
+
         while p_right < len(self.data):
             if self.data[p_right][0] == "break":
                 break
-
             elif self.data[p_right][1] == "ls":
-                p_right += 1
-                p_left = p_right
-                while self.data[p_right][0] != "$" and p_right < len(self.data) - 1:
-                    p_right += 1
-
-                for data in self.data[p_left:p_right]:
-                    if data[0] == "dir":
-                        Node(data[1], parent=p_current, size=0, type="dir")
-                    else:
-                        node = Node(
-                            data[1],
-                            parent=p_current,
-                            type="file",
-                            size=int(data[0]),
-                        )
-                continue
+                p_right = self.create_children(p_right, p_current)
 
             elif self.data[p_right][1] == "cd":
                 if self.data[p_right][2] == "..":
                     p_current = p_current.parent
-
                 else:
-                    children = [
-                        child.name[: len(self.data[p_right][2])]
-                        for child in p_current.children
-                    ]
-                    index = children.index(self.data[p_right][2])
-                    p_current = p_current.children[index]
-
+                    p_current = self.cd_to_child(p_right, p_current)
                 p_right += 1
-
-        # print(RenderTree(self.root))  # use this to see tree
+        # print(RenderTree(self.root))  # use this to see tree in terminal
 
     def solve_part_1(self, node):
         dir_size = 0
@@ -62,14 +91,12 @@ class Solution:
                 dir_size += child.size
 
         node.size = dir_size
-
         if dir_size <= 100000:
             self.part1 += dir_size
-
         return dir_size
 
     def solve_part_2(self):
-        unused_space = 70_000_000 - self.root.size
+        unused_space = self.max_space - self.root.size
         self.space_needed -= unused_space
         self.file_to_delete = self.root.size
 
