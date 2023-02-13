b9433768f1ce8145bcc1a0774c70aec95a63d19a
Gerardo Ayes <gerardoayes92@gmail.com>
2023-02-13 07:50:21 -0500

    I think I have successfully made a graph DS using igraph for Puzzle 12

--- day_12/puzzle_12.py
+++ day_12/puzzle_12.py
@@ -1,32 +1,50 @@
+import igraph as ig
+
+
 class Solution:
     def __init__(self, file: str):
         self.file = file
-        self.data = []
-        self.unvisited = set()
+        letters = "abcdefghijklmnopqrstuvwxyz"
+        self.values = {letters[i]: i for i in range(len(letters))}
+        self.values["S"] = 0
+        self.values["E"] = 25
         self.read_file()
 
     def read_file(self):
-        letters = "abcdefghijklmnopqrstuvwxyz"
-        values = {letters[i]: i for i in range(len(letters))}
+        names, values, edges = [], [], []
 
         with open(self.file) as file:
             file = list(file.readlines())
+            p_curr = 0
+            vertices = len(file) * len(file[-1])
 
             for row in range(len(file)):
                 file[row] = file[row].strip()
-                self.data.append([])
-
-                if "S" in file[row]:
-                    self.start = (row, file[row].index("S"))
-                    file[row] = file[row].replace("S", "a")
-
-                elif "E" in file[row]:
-                    self.end = (row, file[row].index("E"))
-                    file[row] = file[row].replace("E", "z")
 
                 for col in range(len(file[row])):
-                    self.unvisited.add((row, col))
-                    self.data[row].append(values[file[row][col]])
+                    if file[row][col] == "S":
+                        self.start = (row, col)
+
+                    elif file[row][col] == "E":
+                        self.end = (row, col)
+
+                    if row != 0:
+                        edges.append([p_curr, p_curr - len(file[row])])
+                    if row != len(file) - 1:
+                        edges.append([p_curr, p_curr + len(file[row])])
+                    if col != 0:
+                        edges.append([p_curr, p_curr - 1])
+                    if col != len(file[row]) - 1:
+                        edges.append([p_curr, p_curr + 1])
+
+                    names.append(f"{row}, {col}")
+                    values.append(self.values[file[row][col]])
+                    p_curr += 1
+
+        self.data = ig.Graph(n=vertices, edges=edges)
+        self.data.vs["name"] = names
+        self.data.vs["value"] = values
 
     def solve_part_1(self):
+        print(self.data)
         return
